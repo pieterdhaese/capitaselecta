e4f1dd4bd79890886b02bc3ef94e389f1d956d1d
pieter dhaese <pieter.dhaese@student.ehb.be>
2012-05-03 09:46:24 +0200

    reg
    Signed-off-by: pieter dhaese <pieter.dhaese@student.ehb.be>

--- includes/connectvars.php
+++ includes/connectvars.php
@@ -1,7 +1,7 @@
 <?php 
 	//define connections variables
 	define ("DB_SERVER", "a.db.shared.orchestra.io");
-	define ("DB_USER", "user_2f838938");
-	define ("DB_PASS", "8KPU$^lv_Dt%Dd");
-	define ("DB_NAME", "db_2f838938");
+	define ("DB_USER", "user_d83b915c");
+	define ("DB_PASS", ")q0d.66)4)h!d4");
+	define ("DB_NAME", "db_d83b915c");
 ?>
--- index.php
+++ index.php
@@ -10,11 +10,9 @@ session_start();
 				<pre>
 <h1>e-mailadres:</h1><input name="email" type="text" size="25" />
 <h1>wachtwoord:</h1><input name="wachtwoord" type="password" size="15" />
-					<input type="hidden" name="verzonden" value"1" />
-                	<input type="hidden" name="datum" value="<?php echo date ("YmdHis"); ?> " />
                 </pre>
 					<input type="submit" name="submit" value="Inloggen" />
-					<input type="reset" name="Wissen" value="Reset" />
+					<a href="register.php">registreer</a>
                     
     </form>
 </body>
