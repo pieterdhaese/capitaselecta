fbec05eced78b27e933002a7f564b97231351fa2
pieter dhaese <pieter.dhaese@student.ehb.be>
2012-05-07 03:19:24 +0200

    a

--- includes/connectvars.php
+++ includes/connectvars.php
@@ -1,5 +1,5 @@
 <?php 
-	define connections variables
+	//define connections variables
 	define ("DB_SERVER", "a.db.shared.orchestra.io");
 	define ("DB_USER", "user_d83b915c");
 	define ("DB_PASS", ")q0d.66)4)h!d4");
