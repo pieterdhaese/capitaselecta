d41069ef207681cee3641a6c18e58db8221c8ce5
pieter dhaese <pieter.dhaese@student.ehb.be>
2012-05-07 03:16:29 +0200

    aangepast connectvars

--- includes/connectvars.php
+++ includes/connectvars.php
@@ -1,11 +1,11 @@
 <?php 
-	//define connections variables
-	// define ("DB_SERVER", "a.db.shared.orchestra.io");
-	// define ("DB_USER", "user_d83b915c");
-	// define ("DB_PASS", ")q0d.66)4)h!d4");
-	// define ("DB_NAME", "db_d83b915c");
-	define ("DB_SERVER", "127.0.0.1");
-	define ("DB_USER", "pie");
-	define ("DB_PASS", "test");
-	define ("DB_NAME", "capita");
+	define connections variables
+	define ("DB_SERVER", "a.db.shared.orchestra.io");
+	define ("DB_USER", "user_d83b915c");
+	define ("DB_PASS", ")q0d.66)4)h!d4");
+	define ("DB_NAME", "db_d83b915c");
+	// define ("DB_SERVER", "127.0.0.1");
+	// define ("DB_USER", "pie");
+	// define ("DB_PASS", "test");
+	// define ("DB_NAME", "capita");
 ?>
